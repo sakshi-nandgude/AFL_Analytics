0f04597a6d83756d4f112aca266d302a3927d863
sakshinandgude6-web <sakshinandgude6@gmail.com>
2026-06-17 11:26:29 +0100

    Created validate_data.py

---
+++ scripts/validate_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+teams = pd.read_csv("data/raw/teams.csv")
+players = pd.read_csv("data/raw/players.csv")
+matches = pd.read_csv("data/raw/matches.csv")
+stats = pd.read_csv("data/raw/player_stats.csv")
+
+print("Teams:", len(teams))
+print("Players:", len(players))
+print("Matches:", len(matches))
+print("Player Stats:", len(stats))
+
+print("\nNull Values")
+print(teams.isnull().sum())
+print(players.isnull().sum())
+print(matches.isnull().sum())
+print(stats.isnull().sum())
