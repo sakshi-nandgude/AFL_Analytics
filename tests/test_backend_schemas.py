9e2fdd614cf1f36f21c60a7b4e497bcd257ca38f
sakshinandgude6-web <sakshinandgude6@gmail.com>
2026-07-01 15:19:31 +0100

    Created test file for schemas.py for backend

---
+++ tests/test_backend_schemas.py
@@ -0,0 +1,15 @@
+from backend.app.schemas import (
+    DashboardSummarySchema,
+    TeamPerformanceSchema,
+    PlayerSummarySchema,
+    TeamRankingSchema,
+    MatchSummarySchema
+)
+
+print("Schemas Imported Successfully!")
+
+print(DashboardSummarySchema)
+print(TeamPerformanceSchema)
+print(PlayerSummarySchema)
+print(TeamRankingSchema)
+print(MatchSummarySchema)
