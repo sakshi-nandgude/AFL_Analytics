918976bb7ef9b405188f210988fee0046ff8c7c1
sakshinandgude6-web <sakshinandgude6@gmail.com>
2026-07-01 15:25:44 +0100

    Created dashboard_service.py

---
+++ backend/app/services/dashboard_service.py
@@ -0,0 +1,14 @@
+"""
+dashboard_service.py
+
+Contains business logic for the dashboard.
+"""
+
+from sqlalchemy.orm import Session
+
+from backend.app.models import DashboardSummary
+
+
+def get_dashboard_summary(db: Session):
+
+    return db.query(DashboardSummary).first()
